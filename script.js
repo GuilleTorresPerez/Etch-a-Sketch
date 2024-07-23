4f0b80c84658975a8a4e4a568ebb84e7d865abd8
Guillermo Torres Pérez <60155590+GuilleTorresPerez@users.noreply.github.com>
2024-07-23 18:55:44 +0200

    Create dynamic grid

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
+let gridWidth = 16;
+let gridSize = gridWidth * gridWidth;
+let boardWidth = 750;
+let boardSize = boardWidth * boardWidth;
 
 document.addEventListener('DOMContentLoaded', function() {
     //Create grid
-    const container = document.querySelector('.container');
-    
-    for (let i = 0; i < 16; i++) {
-        for(let i = 0; i < 16; i++) {
+    const board = document.querySelector('.board');
+    board.style.width = boardWidth + 'px';
+
+    const cellSize = Math.floor(boardSize/gridSize);
+    console.log('cell size is: ' + cellSize);
+    const cellWidth = Math.sqrt(cellSize)
+    console.log('cell width is: ' + cellWidth);
+    for (let i = 0; i < gridWidth; i++) {
+        for(let i = 0; i < gridWidth; i++) {
             const cell = document.createElement('div');
             cell.className = 'cell';
-            container.appendChild(cell);
+            console.log('width: ' + cell.style.width);
+            board.appendChild(cell);
         }
     }
-
+    
     const cells = document.querySelectorAll('.cell');
     cells.forEach((cell) => {
-        cell.style.width = '20px';
+        cell.style.flex = '1 0 auto';
+        cell.style.width = cellWidth+'px';
         cell.style.aspectRatio = '1 / 1';
-        // cell.style.border = 'solid';
+        cell.style.boxSizing = 'border-box';
+
+        cell.style.border = 'solid';
+        cell.style.borderWidth = 'thin';
     });
 }); 
